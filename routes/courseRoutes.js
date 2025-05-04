e724abe6c8fdea9b193eb6cd9086ec6118018f31
Suraj Pawar <suraj.leadsoft@gmail.com>
2025-05-04 10:58:50 +0530

    main

--- routes/courseRoutes.js
+++ routes/courseRoutes.js
@@ -6,8 +6,8 @@ const Student = require('../models/Student')
 // POST: Insert new course
 router.post('/insert', async (req, res) => {
   try {
-    const { courseName } = req.body;
-    const course = new Course({ courseName });
+    const { courseName,courseFees } = req.body;
+    const course = new Course({ courseName,courseFees });
     await course.save();
     res.status(201).json({ success: true, message: 'Course inserted', data: course });
   } catch (error) {
